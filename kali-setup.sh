066f1bc5a319517543968c6febeb62fa99ff4949
Blake <blake@MBP.local>
2024-08-13 18:21:02 -0600

    Added y flag to brave install

--- kali-setup.sh
+++ kali-setup.sh
@@ -28,4 +28,4 @@ sudo ./pimpmykali.sh --all
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install brave-browser -y
